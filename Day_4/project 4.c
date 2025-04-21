6742b3e2b236e710ed84b6815ab4c717d732ab88
Ganesh <ganesheart1@gmail.com>
2025-04-21 20:09:37 +0530

    Add files via upload

---
+++ Day_4/project 4.c
@@ -0,0 +1,29 @@
+#include<reg51.h>
+sbit wave = P1^1;
+void delay();
+void main ()
+{
+	wave = 0;
+	while(1) 
+		{
+			wave = 1;
+			delay();
+			wave = 0;
+			delay();
+		}
+		
+	}
+
+	void delay () 
+	{
+		unsigned char i;
+		TMOD = 0X10;
+		for(i=0 ; i<14 ; i++)
+		{
+			TH1 = 0X00;
+			TL1 = 0X00;
+			TR1 = 1;
+			while(TF1 == 0);
+			TF1=0;
+		}
+		}
